511bc2cc1a271e450f7860c6bd11120974f45881
g-patin <g.patin@monemail.com>
2024-09-22 16:19:55 +0200

    added add_new_person function to init file

--- src/microfading/__init__.py
+++ src/microfading/__init__.py
@@ -1,2 +1,2 @@
-from .microfading import MFT, get_datasets, process_rawdata, create_DB, folder_DB, get_DB, update_DB_objects, update_DB_projects, add_new_project, add_new_object
+from .microfading import MFT, get_datasets, process_rawdata, create_DB, folder_DB, get_DB, update_DB_objects, update_DB_projects, add_new_project, add_new_object, add_new_person
 
